d4c23d797be91a6a176c8ed2331bcc01ae443cab
Alexandre <alex301097@hotmail.fr>
2024-10-20 12:44:08 +0200

    Correction setup

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ enableCORS=false\n\
 port = $PORT\n\
 \n\
 [theme]\n\
-primaryColor="#031775"\n\
-backgroundColor="#FFFFFF"\n\
-secondaryBackgroundColor="#8D8FA5"\n\
-textColor="#242424"\n\
+primaryColor=\"#031775\"\n\
+backgroundColor=\"#FFFFFF\"\n\
+secondaryBackgroundColor=\"#8D8FA5\"\n\
+textColor=\"#242424\"\n\
 font=\"Custom Pret à dépenser\"\n\
 " > ~/.streamlit/config.toml
